f21a5a9331830e442ee95cd63d1bf80d6521057d
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-07-06 14:24:34 +0200

    server

--- server.js
+++ server.js
@@ -33,10 +33,14 @@ app.use('*', (req, res) => {
 
 
 /* MONGOOSE */
-dbUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.epgpr.mongodb.net/mariana?retryWrites=true&w=majority`;
- 
- 
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+
+if(NODE_ENV === 'production') dbUri = `mongodb+srv://JUSTI:test123@cluster0.epgpr.mongodb.net/mariana?retryWrites=true&w=majority`;
+  else dbUri = 'mongodb://localhost:27017/mariana';
+
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.once('open', () => {
